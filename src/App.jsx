d065152ac12270658307ce5b41490433e0e04ebc
Pankaj Bora <pankajbora1111@gmail.com>
2024-05-02 20:54:38 +0530

    created home

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
-
+import Home from './components/Home'
 function App() {
   
 
   return (
     <>
+    <Home />
+  
     </>
   )
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+
+const RoomComponent = () => {
+  const [name, setName] = useState('');
+  const [roomName, setRoomName] = useState('');
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleRoomChange = (e) => {
+    setRoomName(e.target.value);
+  };
+
+  const joinRoom = () => {
+    
+    console.log(`Joining room ${roomName} as ${name}`);
+  };
+
+  const createRoom = () => {
+ 
+    
+    console.log(`Creating room ${roomName} with host ${name}`);
+  };
+
+  return (
+    <div>
+      <label htmlFor="name">Name:</label>
+      <input
+        type="text"
+        id="name"
+        value={name}
+        onChange={handleNameChange}
+        placeholder="Enter your name"
+      />
+      <br />
+      <input
+        type="text"
+        id="roomName"
+        value={roomName}
+        onChange={handleRoomChange}
+        placeholder="Enter room name"
+      />
+      <br />
+      <button onClick={joinRoom}>Join Room</button>
+      <button onClick={createRoom}>Create Room</button>
+    </div>
+  );
+};
+
+export default RoomComponent;
